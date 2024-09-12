de0430608fe97dd2776c04ae17d113da2d05a88a
Abriel <abrielz11@gmail.com>
2024-09-13 01:00:13 +0300

    replaced direct impl bean of calc to interface + class and data for test are created

---
+++ useCase/src/main/java/ru/fastdelivery/usecase/TariffCalculateUseCaseImpl.java
@@ -0,0 +1,161 @@
+package ru.fastdelivery.usecase;
+
+import lombok.RequiredArgsConstructor;
+import ru.fastdelivery.domain.common.currency.CurrencyFactory;
+import ru.fastdelivery.domain.common.dimesions.CargoDimensions;
+import ru.fastdelivery.domain.common.dimesions.Length;
+import ru.fastdelivery.domain.common.price.Price;
+import ru.fastdelivery.domain.common.weight.Weight;
+import ru.fastdelivery.domain.delivery.pack.Pack;
+import ru.fastdelivery.domain.delivery.shipment.Shipment;
+import javax.inject.Named;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Comparator;
+import org.springframework.beans.factory.annotation.Value;
+
+@Named
+@RequiredArgsConstructor
+public class TariffCalculateUseCaseImpl implements TariffCalculateUseCase {
+
+    private final CurrencyFactory currencyFactory;
+
+    @Value("${cost.rub.perKg}")
+    Integer costPerKgs;
+
+    @Override
+    public Price calculatorPriceByCargoWeight(Shipment shipment) {
+
+        this.isValidate(shipment);
+        this.computerOfVolumes(shipment);
+        this.computerOfPriceFullVolumeOfPackages(shipment);
+
+        shipment.setTotalPrice(shipment.getTotalVolumeOfCargo() * shipment.getTotalVolumeOfCargo());
+
+        shipment.getPackages().sort(Comparator.comparing(Pack::getWeight)
+                .thenComparing(Pack::getPricePerCargoUnit));
+
+        BigDecimal maxPrice = BigDecimal.valueOf(
+                shipment.getPackages().get(shipment.getPackages().size() - 1).getPricePerCargoUnit());
+
+        return new Price(maxPrice, shipment.getCurrency());
+    }
+
+    private void isValidate(Shipment shipment) {
+        var currency = currencyFactory.create(
+                shipment.getCurrency().getCode());
+
+        if (!currency.getCode().equals("RUB")) {
+            throw new RuntimeException("Your currency is not supported yet~!");
+        }
+
+        shipment.getPackages().forEach(t -> {
+
+            if (!t.getWeight().greaterThan(new Weight(new BigInteger(String.valueOf(150000))))) {
+                throw new RuntimeException("Weight of Package is more then 1500 kg");
+            }
+
+            this.dimensionsLimiterChecker(shipment);
+        });
+    }
+
+    private void dimensionsLimiterChecker(Shipment shipment) {
+
+        for (Pack cargoUnit : shipment.getPackages()) {
+            if (cargoUnit.getDimensions().getHeight().length() > 1500 ||
+                    cargoUnit.getDimensions().getHeight().length() < 1) {
+                throw new RuntimeException("Weight of Package height is more then 1500 cm or less then 1 cm");
+            }
+
+            if (cargoUnit.getDimensions().getWidth().length() > 1500 ||
+                    cargoUnit.getDimensions().getWidth().length() < 1) {
+                throw new RuntimeException("Weight of Package width is more then 1500 cm or less then 1 cm");
+            }
+
+            if (cargoUnit.getDimensions().getLength().length() > 1500 ||
+                    cargoUnit.getDimensions().getLength().length() < 1) {
+                throw new RuntimeException("Weight of Package length is more then 1500 cm or less then 1 cm");
+            }
+        }
+    }
+
+    private void computerOfVolumes(Shipment shipment) {
+
+        this.rounderOfVolumes(shipment);
+
+        for (Pack cargo : shipment.getPackages()) {
+            double volumeOfCargo = ((double)
+                    (cargo.getDimensions().getHeight().length()
+                            * cargo.getDimensions().getLength().length()
+                            * cargo.getDimensions().getWidth().length()
+                    ) / 1_000_000_000);
+
+            cargo.setVolumeOfCargoUnit(Math.round(volumeOfCargo * 10_000d) / 10_000d);
+        }
+    }
+
+    private void computerOfPriceFullVolumeOfPackages(Shipment shipment) {
+
+        Double totalVolumeOfCargo = 0.0d;
+
+        for (Pack cargo : shipment.getPackages()) {
+            cargo.setPricePerCargoUnit(cargo.getVolumeOfCargoUnit() * costPerKgs);
+            totalVolumeOfCargo += cargo.getVolumeOfCargoUnit();
+        }
+
+        shipment.setTotalVolumeOfCargo(totalVolumeOfCargo);
+    }
+
+    private void rounderOfVolumes(Shipment shipment) {
+
+        for (Pack cargo : shipment.getPackages()) {
+            Integer tempValue = cargo.getDimensions().getWidth().length();
+
+            if (tempValue % 50 == 0) {
+                continue;
+            } else {
+
+                tempValue = (tempValue * 50) / 50;
+
+                cargo.setDimensions(new CargoDimensions(
+                        cargo.getDimensions().getLength(),
+                        new Length(tempValue),
+                        cargo.getDimensions().getHeight()
+                ));
+            }
+        }
+
+        for (Pack cargo : shipment.getPackages()) {
+            Integer tempValue = cargo.getDimensions().getLength().length();
+
+            if (tempValue % 50 == 0) {
+                continue;
+            } else {
+
+                tempValue = (tempValue * 50) / 50;
+
+                cargo.setDimensions(new CargoDimensions(
+                        new Length(tempValue),
+                        cargo.getDimensions().getWidth(),
+                        cargo.getDimensions().getHeight()
+                ));
+            }
+        }
+
+        for (Pack cargo : shipment.getPackages()) {
+            Integer tempValue = cargo.getDimensions().getHeight().length();
+
+            if (tempValue % 50 == 0) {
+                continue;
+            } else {
+
+                tempValue = (tempValue * 50) / 50;
+
+                cargo.setDimensions(new CargoDimensions(cargo.getDimensions().getLength(),
+                        cargo.getDimensions().getWidth(),
+                        new Length(tempValue)
+                ));
+            }
+        }
+    }
+}
--- useCase/src/test/java/ru/fastdelivery/usecase/TariffCalculateUseCaseTest.java
+++ useCase/src/test/java/ru/fastdelivery/usecase/TariffCalculateUseCaseTest.java
@@ -1,72 +1,125 @@
 package ru.fastdelivery.usecase;
 
-import org.assertj.core.util.BigDecimalComparator;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.fastdelivery.domain.common.currency.Currency;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.beans.factory.annotation.Value;
 import ru.fastdelivery.domain.common.currency.CurrencyFactory;
 import ru.fastdelivery.domain.common.delivery.Departure;
 import ru.fastdelivery.domain.common.delivery.Destination;
 import ru.fastdelivery.domain.common.destinations.CoordinatesValue;
 import ru.fastdelivery.domain.common.destinations.Latitude;
 import ru.fastdelivery.domain.common.destinations.Longitude;
+import ru.fastdelivery.domain.common.dimesions.CargoDimensions;
+import ru.fastdelivery.domain.common.dimesions.Length;
 import ru.fastdelivery.domain.common.price.Price;
 import ru.fastdelivery.domain.common.weight.Weight;
 import ru.fastdelivery.domain.delivery.pack.Pack;
 import ru.fastdelivery.domain.delivery.shipment.Shipment;
-
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import ru.fastdelivery.domain.common.currency.Currency;
+import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.DisplayName;
+import org.assertj.core.util.BigDecimalComparator;
 
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class TariffCalculateUseCaseTest {
 
-//    final WeightPriceProvider weightPriceProvider = mock(WeightPriceProvider.class);
-//    final Currency currency = new CurrencyFactory(code -> true).create("RUB");
-//
-//    final TariffCalculateUseCase tariffCalculateUseCase = new TariffCalculateUseCase(weightPriceProvider);
-//
-//    @Test
-//    @DisplayName("Расчет стоимости доставки -> успешно")
-//    void whenCalculatePrice_thenSuccess() {
-//        var minimalPrice = new Price(BigDecimal.TEN, currency);
-//        var pricePerKg = new Price(BigDecimal.valueOf(100), currency);
-//
-//        when(weightPriceProvider.minimalPrice()).thenReturn(minimalPrice);
-//        when(weightPriceProvider.costPerKg()).thenReturn(pricePerKg);
-//
-//        var dest1long = new Latitude(new CoordinatesValue(new BigDecimal(200)));
-//        var dest1lat = new Longitude(new CoordinatesValue(new BigDecimal(200)));
-//
-//        var dest2long = new Latitude(new CoordinatesValue(new BigDecimal(200)));
-//        var dest2lat = new Longitude(new CoordinatesValue(new BigDecimal(200)));
-//
-//        var shipment = new Shipment(List.of(new Pack(new Weight(BigInteger.valueOf(1200)))),
-//                new CurrencyFactory(code -> true).create("RUB"),
-//                new Destination(dest1long, dest1lat),
-//                new Departure(dest2long, dest2lat));
-//        var expectedPrice = new Price(BigDecimal.valueOf(120), currency);
-//
-//        var actualPrice = tariffCalculateUseCase.calc(shipment);
-//
-//        assertThat(actualPrice).usingRecursiveComparison()
-//                .withComparatorForType(BigDecimalComparator.BIG_DECIMAL_COMPARATOR, BigDecimal.class)
-//                .isEqualTo(expectedPrice);
-//    }
-//
-//    @Test
-//    @DisplayName("Получение минимальной стоимости -> успешно")
-//    void whenMinimalPrice_thenSuccess() {
-//        BigDecimal minimalValue = BigDecimal.TEN;
-//        var minimalPrice = new Price(minimalValue, currency);
-//        when(weightPriceProvider.minimalPrice()).thenReturn(minimalPrice);
-//
-//        var actual = tariffCalculateUseCase.minimalPrice();
-//
-//        assertThat(actual).isEqualTo(minimalPrice);
-//    }
+    @Mock
+    private CurrencyFactory currencyFactory;
+
+    @InjectMocks
+    public TariffCalculateUseCase tariffCalculateUseCase;
+
+    @Value("${cost.rub.perKg}")
+    private Integer costPerKgs;
+
+    private Currency currency; // +
+
+    private Shipment shipment; // ++
+
+    private Pack cargoUnit0; // ++
+
+    private Pack cargoUnit1; // --
+
+    private Pack cargoUnit2; // --
+
+    private Price price; // --
+
+    private Weight weight; // +
+
+    private Departure departure; // ++
+
+    private Destination destination; // ++
+
+    private CoordinatesValue coordinatesValue; // ++
+
+    private Latitude latitude; // ++
+
+    private Longitude longitude; // ++
+
+    private CargoDimensions cargoDimensions; // ++
+
+    private Length lengthWidth; // ++
+
+    private Length lengthHeight; // ++
+
+    private Length lengthLength; // ++
+
+    private List<Pack> packageofCargoList; // --
+
+    @BeforeEach
+    public void init() {
+        //todo написать инициализацию груза и поставки
+
+        currency = currencyFactory.create("RUB");
+
+        // ============== 1st Package for Shipment ============
+
+        weight = new Weight(BigInteger.valueOf(1000));
+
+        lengthWidth = new Length(1200);
+
+        lengthHeight = new Length(1000);
+
+        lengthLength = new Length(1400);
+
+        destination = new Destination(latitude, longitude);
+
+        departure = new Departure(latitude, longitude);
+
+        coordinatesValue = new CoordinatesValue(BigDecimal.valueOf(0.0d));
+
+        latitude = new Latitude(coordinatesValue);
+
+        longitude = new Longitude(coordinatesValue);
+
+        cargoDimensions = new CargoDimensions(lengthLength, lengthWidth, lengthHeight);
+
+        cargoUnit0 = new Pack(weight, cargoDimensions, (double) costPerKgs, 0.0d);
+
+        // ============== 1st Package for Shipment ============
+
+        packageofCargoList = List.of(cargoUnit0);
+
+        shipment = new Shipment(packageofCargoList, 0.0d, 0.0d, currency, destination, departure);
+    }
+
+    @Test
+    void calculatorPriceByCargoWeight() {
+
+    }
 }
